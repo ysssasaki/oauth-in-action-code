e1e43af94341198d93d982500b66f8fa97318a05
ysssasaki <yss.sasaki@w.ntt.com>
2022-07-21 17:56:03 +0900

    Support refresh token

--- exercises/ch-5-ex-2/authorizationServer.js
+++ exercises/ch-5-ex-2/authorizationServer.js
@@ -162,14 +162,14 @@ app.post("/token", function(req, res){
 				var access_token = randomstring.generate();
 				nosql.insert({ access_token: access_token, client_id: clientId });
 
-				/*
-				 * Issue a refresh token along side the access token and save it to the database
-				 */
+				var refresh_token = randomstring.generate();
+				nosql.insert({ refresh_token: refresh_token, client_id: clientId });
+
 
 				console.log('Issuing access token %s', access_token);
 				console.log('with scope %s', code.scope);
 
-				var token_response = { access_token: access_token, token_type: 'Bearer' };
+				var token_response = { access_token: access_token, token_type: 'Bearer',  refresh_token: refresh_token };
 
 				res.status(200).json(token_response);
 				console.log('Issued tokens for code %s', req.body.code);
@@ -187,10 +187,43 @@ app.post("/token", function(req, res){
 			return;
 		}
 
-	/*
-     * Respond to a refresh token request by issuing a new access token
-	 */
-		
+	} else if (req.body.grant_type == 'refresh_token') {
+
+		nosql.one(function(token) {
+			if (token.refresh_token == req.body.refresh_token) {
+				return token;
+			}
+		}, function(err, token) {
+			if (token) {
+				if (token.client_id != clientId) {
+					nosql.remove(function(found) {
+						return (found == token);
+					}, function () {} );
+
+					res.status(400).json({error: 'invalid_grant'});
+					return;
+				}
+
+				var access_token = randomstring.generate();
+				nosql.insert({
+					access_token: access_token,
+					client_id: clientId
+				});
+
+				var token_response = {
+					access_token: access_token,
+					token_type: 'Bearer',
+					refresh_token: token.refresh_token,
+				};
+
+				res.status(200).json(token_response);
+				return;
+			} else {
+				res.status(400).json({error: 'invalid_grant'})
+				return;
+			}
+		});
+	
 	} else {
 		console.log('Unknown grant type %s', req.body.grant_type);
 		res.status(400).json({error: 'unsupported_grant_type'});
